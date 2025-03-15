d4e86aae873559752875d1367038ec9997604d85
NicolasGCADS <nguinante@gmail.com>
2025-03-15 19:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ Livro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    public abstract class Livro  
+    {
+        
+        public abstract string Sinopse();
+    }
+
+    class Terror : Livro
+    {
+        
+        public override string Sinopse()
+        {
+            return "O jovem advogado inglês Jonathan Harker aceita um trabalho em uma vila sombria nas brumas da Transilvânia, na Romênia. " +
+                   "Ele é capturado e aprisionado pelo vampiro Drácula, que viaja para Londres inspirado por uma fotografia da noiva de Harker, Mina Murray. " +
+                   "Na Grã-Bretanha, Drácula começa um reinado de sedução e horror, drenando a vida da amiga mais próxima de Mina, Lucy Westenra. " +
+                   "Os amigos de Lucy unem forças com o professor Abraham Van Helsing para tentar afastar Drácula de uma vez por todas.";
+        }
+    }
+
+    
+    class LivroTeste
+    {
+        static void Main()
+        {
+         
+            Livro livro = new Terror();
+
+            
+            Console.WriteLine(livro.Sinopse());
+        }
+    }
+
---
+++ Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Usuario
+{
+    private int id;
+    private String nome;
+    private int cpf;
+    private int idade;
+    private String livrosComprados;
+    private String sugestoes;
+    private String saldo;
+}
