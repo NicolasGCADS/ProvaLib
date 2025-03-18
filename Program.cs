56bdc097d8fb8a2d516603cfe3a74c45252d34b0
NicolasGCADS <nguinante@gmail.com>
2025-03-17 22:37:42 -0300

    Enviando console junto

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Reflection.Metadata;
 using System.Xml.Serialization;
 using System;
 
-var Sinopse = "O jovem advogado inglês Jonathan Harker aceita um trabalho em uma vila sombria nas brumas da Transilvânia, na Romênia. \" \r\n                   \"Ele é capturado e aprisionado pelo vampiro Drácula, que viaja para Londres inspirado por uma fotografia da noiva de Harker, Mina Murray. \" \r\n                   \"Na Grã-Bretanha, Drácula começa um reinado de sedução e horror, drenando a vida da amiga mais próxima de Mina, Lucy Westenra. \" \r\n                   \"Os amigos de Lucy unem forças com o professor Abraham Van Helsing para tentar afastar Drácula de uma vez por todas.";
+var Sinopse = "O jovem advogado inglês Jonathan Harker aceita um trabalho em uma vila sombria nas brumas da Transilvânia, na Romênia. \" \r\n                   \"Ele é capturado e aprisionado pelo vampiro Drácula, que viaja para Londres inspirado por uma fotografia da noiva de Harker, Mina Murray. \" \r\n                   \"Na Grã-Bretanha, Drácula começa um reinado de sedução e horror, drenando a vida da amiga mais próxima de Mina, Lucy Westenra. \" \r\n                   \"Os amigos de Lucy unem forças com o professor Abraham Van Helsing para tentar afastar Drácula de uma vez por todas";
 
 Console.WriteLine("Esse é seu livro?");
 Console.WriteLine(Sinopse);
